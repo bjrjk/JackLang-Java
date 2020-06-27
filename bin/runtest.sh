e47a9df52b85ee4f2ef9823ac1b5f43c3087ab81
Jack Ren <bjrjk@qq.com>
2020-06-27 19:31:57 +0800

    Fix mods

--- bin/runtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export CLASSPATH=".:/usr/local/lib/antlr-4.8-complete.jar:$CLASSPATH"
-alias antlr4='java -jar /usr/local/lib/antlr-4.8-complete.jar'
-alias grun='java org.antlr.v4.gui.TestRig'
-
-javac *.java
-grun JackLang prog -gui
